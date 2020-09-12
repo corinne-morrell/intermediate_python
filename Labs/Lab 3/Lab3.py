2082833fd2d3cbd07340c547445d119fad19b4fd
Corinne Hatley <corinnehatley@Corinnes-Air.lan1>
2020-09-11 20:09:18 -0400

    it's alive! lab 3

--- Labs/Lab 3/Lab3.py
+++ Labs/Lab 3/Lab3.py
@@ -5,31 +5,38 @@
 
 import matplotlib as plt
 
-def calcTime(time, dt, acc_init, exhaust):
-    '''Generates a range of time values from 0 to 45 seconds in 0.1 s intervals and appends the values to a list'''
-    t_previous = 0
-    t_current = []
-    for time in range(10):
-        t_previous = t_previous + dt
-        t_current.append(t_previous)
-    return t_current
+#def calcTrajectory(vel_previous_pred, acc_init_pred, exh_pred, dis_init_pred, vel_max_prey, acc_init_prey, exh_prey, dis_init_prey, t_previous, delta_t):
 
+time_current = [0.0]
+acc_current_cheetah = [10.0]
+vel_current_cheetah = [0.0]
+dis_current_cheetah = [0.0]
 
-def calcPredator_displacement(acc_init, exhaust):
-    time = t_current
-    acc_current = acc_init + exhaust * time
-    print(acc_current)
+vel_init_cheetah = 0.0
+vel_max_cheetah = 29.0
+acc_init_cheetah = 10.0
+exhaustion_cheetah = -0.55
+dis_init_cheetah = 0.0
 
-#def calcPrey_displacement():
+delta_time = 0.1
+total_time = 20
 
-def main():
-    t_init = 0
-    dt = 0.1
-    t_current = calcTime(t_init, dt)
+for t in range (0, 20):
+    updated_time = time_current[t] + delta_time
+    time_current.append(updated_time)
+    
+    updated_acc_cheetah = acc_init_cheetah + (exhaustion_cheetah * time_current[t])
+    acc_current_cheetah.append(updated_acc_cheetah)
+    
+    updated_vel_cheetah = max(0, min(vel_init_cheetah + acc_current_cheetah[t] * delta_time, vel_max_cheetah))
+    vel_current_cheetah.append(updated_vel_cheetah)
+    vel_init_cheetah = updated_vel_cheetah
 
-    calcPredator_displacement(10, -0.55)
+    updated_dis_cheetah = dis_init_cheetah + vel_current_cheetah[t] * delta_time
+    dis_current_cheetah.append(updated_dis_cheetah)
 
-    #plt.plot(t_current, dis_predator)
-    #plt.plot(t_current, dis_prey)
-
-main()
+    
+print(time_current)
+print(acc_current_cheetah)
+print(vel_current_cheetah)
+print(dis_current_cheetah)
---
+++ Labs/Lab 3/Lab3outtakes.py
@@ -0,0 +1,36 @@
+# Cori Hatley
+# 09-15-20
+# Visualize predator-prey capture dynamics with matplotlib
+# To run, enter python Lab3.py
+
+import matplotlib as plt
+
+def calcTime(time, dt):
+    '''Generates a range of time values from 0 to 45 seconds in 0.1 s intervals and appends the values to a list'''
+    t_previous = 0.0
+    t_current = [0.0]
+    for time in range(10):
+        t_previous = t_previous + dt
+        t_current.append(t_previous)
+    return t_current
+
+
+def calcPredator_displacement(time):
+    exhaust = [-0.55]
+    acc_init = [10.0]
+    acc_current = acc_init + exhaust * time
+    print(acc_current)
+
+#def calcPrey_displacement():
+
+def main():
+    t_init = 0.0
+    delta_t = 0.1
+    t_c = calcTime(t_init, delta_t)
+
+    calcPredator_displacement(t_c)
+
+    #plt.plot(t_current, dis_predator)
+    #plt.plot(t_current, dis_prey)
+
+main()
