ca121a6d64b782c2bf07e3aa35c7bed15dc309e6
Corinne Hatley <corinnehatley@Corinnes-Air.lan1>
2020-09-23 15:07:56 -0400

    integrated num_pairs constraint

--- Labs/Lab 5/Lab5.py
+++ Labs/Lab 5/Lab5.py
@@ -2,7 +2,7 @@
 # 09-29-2020
 # Endangered Species Population Dynamics
 # Objective 1: Model a colony of emperor penguins over the course of 200 years.
-# Objective 2: Consider how "warm events" impact extinction and calculate average extinction over 1000 trials.
+# Objective 2: Consider how "warm events" impact extinction and calculate average extinction over 100 trials.
 # Objective 3: Determine the probability of extinction in each possible year.
 # Objective 4: Visualize results by plotting years (0-200) versus probability of extinction.
 # To run this program in the terminal, enter 'python Lab5.py'
@@ -25,11 +25,6 @@ def sim_one_year():
         current_pop.append('f')
         females += 1
 
-    # Count breeding pairs
-    num_m = current_pop.count('m')
-    num_f = current_pop.count('f')
-    num_pairs = min(num_m, num_f)
-
     # Factor in temperature and establish survival rates for warm/non-warm years
     warm_rate = 0.18
     if random.random() < warm_rate:
@@ -44,7 +39,7 @@ def sim_one_year():
         chick_survival = 0.19
         adult_survival = 0.9
 
-    # Create next_pop list
+    # Initialize next population
     next_pop = []
 
     # If adult survives (based on survival rate determined by warm/non-warm year), append the adult from current_pop to next_pop
@@ -54,10 +49,19 @@ def sim_one_year():
             next_pop.append(current_pop[index])
         index += 1
 
-    # If chick survives (based on survival rate determined by warm/non-warm year), append a randomly-gendered chick to next_pop
-    if random.random() < chick_survival:
-        chick = random.choice(["m", "f"])
-        next_pop.append(chick)
+    # Count breeding pairs
+    num_m = next_pop.count('m')
+    num_f = next_pop.count('f')
+    num_pairs = min(num_m, num_f)
+    print(num_pairs)
+
+   # If chick survives (based on survival rate determined by warm/non-warm year), append a randomly-gendered chick to next_pop
+    breeding_pairs = 0
+    while breeding_pairs <= num_pairs:
+        if random.random() < chick_survival:
+            chick = random.choice(['m', 'f'])
+            next_pop.append(chick)
+        breeding_pairs += 1
 
     print(next_pop)
 sim_one_year()
