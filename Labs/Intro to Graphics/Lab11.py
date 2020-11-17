c42f1f1f7d222326fe9ff8127c73547105d1c404
Corinne Hatley <corinnehatley@Corinnes-MacBook-Air.local>
2020-11-17 13:17:34 -0500

    orbiting themselves???

--- Labs/Intro to Graphics/Lab11.py
+++ Labs/Intro to Graphics/Lab11.py
@@ -15,6 +15,20 @@ import math
 import time
 import sys
 
+class Sun(gr.Circle):
+	def __init__(self, center, radius, color, name, mass):
+		x, y = center
+		gr.Circle.__init__(self, gr.Point(x, y), radius)
+		self.setFill(color)
+		self.name = gr.Text(gr.Point(x, y), name)
+		self.name.setTextColor("black")
+		self.mass = mass
+
+	def getName(self):
+		return self.name
+
+	def getMass(self):
+		return self.mass
 class Planet(gr.Circle):
 	'''
 	Creates a Planet object with a center, radius (in pixels), color, name, semimajor axis length (in pixels),
@@ -45,8 +59,7 @@ class Planet(gr.Circle):
 		return self.name
 
 	def getAxis(self):
-		scaledAxis = self.semimajor_axis
-		return scaledAxis
+		return self.semimajor_axis
 
 	def getEcc(self):
 		return self.eccentricity
@@ -55,16 +68,14 @@ class Planet(gr.Circle):
 		return self.path_color
 
 	def getMass(self):
-		scaledMass = self.mass
-		return scaledMass
+		return self.mass
 
 	def getVel(self):
-		scaledVel = self.v_aphelion
-		return scaledVel
+		return self.v_aphelion
 
-	def enable(self, win): # draw the Planet object in the window
-		self.draw(win)
-		self.name.draw(win)
+	def enable(self, space): # draw the Planet object in the window
+		self.draw(space)
+		self.name.draw(space)
 
 	def disable(self): # undraw the Planet object
 		self.undraw()
@@ -72,36 +83,40 @@ class Planet(gr.Circle):
 
 	def orbit(self, sun):
 
-		
-		solar_mass = 1.989e30
-
 		G = 6.67428e-11
 
-		dtheta = 0.001
+		dtheta = 0.1
 		dtime = 0.01
 
 		self.theta = (self.theta + dtheta) % (2 * math.pi)
 		self.time = self.time + dtime
 
-		self.r = (self.getAxis() * (1 - (self.getEcc())**2)) / (1 + self.getEcc() * math.cos(self.theta))
-		self.r_prime = math.sqrt(self.r**2 + (4 * self.getAxis() * self.getEcc() * (self.getAxis() * self.getEcc() + self.r * math.cos(self.theta))))
+		#self.r = (self.getAxis() * (1 - (self.getEcc())**2)) / (1 + self.getEcc() * math.cos(self.theta))
+		#self.r_prime = math.sqrt(self.r**2 + (4 * self.getAxis() * self.getEcc() * (self.getAxis() * self.getEcc() + self.r * math.cos(self.theta))))
 
-		self.f_gravity = G * self.getMass() * solar_mass / ((self.r_prime)**2)
+		sun.sunX = sun.getCenter().getX()
+		sun.sunY = sun.getCenter().getY()		
+		
+		self.currentX = (self.getCenter().getX()) / 10**4
+		self.currentY = (self.getCenter().getY()) / 10**4
+
+		self.distX = sun.sunX - self.currentX
+		self.distY = sun.sunY - self.currentY
+		self.dist = math.sqrt((self.distX)**2 + (self.distY)**2)
+
+		self.f_gravity = (G * self.getMass() * sun.getMass()) / ((self.dist)**2)
 
 		self.fx = math.cos(self.theta) * self.f_gravity
 		self.fy = math.sin(self.theta) * self.f_gravity
 
-		self.velX = math.cos(self.theta) * self.getVel()
-		self.velY = math.sin(self.theta) * self.getVel()
+		self.velX = (math.cos(self.theta) * self.getVel()) / 10**15
+		self.velY = (math.sin(self.theta) * self.getVel()) / 10**15
 
-		self.accX = self.fx / self.getMass()
-		self.accY = self.fy / self.getMass()
+		self.accX = (self.fx / self.getMass()) / 10**15
+		self.accY = (self.fy / self.getMass()) / 10**15
 
-		self.currentX = (self.getCenter().getX()) / 10**4
-		self.currentY = (self.getCenter().getY()) / 10**4
-
-		self.newX = self.currentX + (self.velX * self.time) + ( 0.5 * self.accX * (self.time)**2)
-		self.newY = self.currentY + (self.velY * self.time) + ( 0.5 * self.accY * (self.time)**2)
+		self.newX = (self.currentX + (self.velX * self.time) + ( 0.5 * self.accX * (self.time)**2))
+		self.newY = (self.currentY + (self.velY * self.time) + ( 0.5 * self.accY * (self.time)**2))
 
 		self.dx = self.newX - self.currentX
 		self.dy = self.newY - self.currentY
@@ -109,8 +124,14 @@ class Planet(gr.Circle):
 		self.move(self.dx, self.dy)
 		self.name.move(self.dx, self.dy)
 
-		sys.stdout.write("\rcurrent position: {0}, {1}\ndistance from sun: {2}\nforce gravity: {3}\nacceleration: {4}, {5}\nnew position: {6}, {7}\ndx, dy: {8}, {9}".format(self.currentX, self.currentY, self.r_prime, self.f_gravity, self.accX, self.accY, self.newX, self.newY, self.dx, self.dy))
-		sys.stdout.flush()
+		print("current position: {0}, {1}".format(self.currentX, self.currentY))
+		print("new position: {0}, {1}".format(self.newX, self.newY))
+		print("dx, dy: {0}, {1}".format(self.dx, self.dy))
+		print("distance from sun: {0}".format(self.dist))
+		print("force of gravity: {0}".format(self.f_gravity))
+		print("velocity: {0}, {1}".format(self.velX, self.velY))
+		print("acceleration: {0}, {1}".format(self.accX, self.accY))
+		print("theta: {0}".format(self.theta))
 
 
 		'''# Temporarily undraw Mars and plot orbit path
@@ -125,14 +146,10 @@ class Planet(gr.Circle):
 			planet_path = gr.Point(planet_centerX, planet_centerY)
 			color = planet.getPath()
 			planet_path.setFill(color)
-			planet_path.draw(win)
+			planet_path.draw(space)
 
 		# Re-draw Mars on top of orbit path
-		planet.enable(win)'''
-
-
-
-
+		planet.enable(space)'''
 
 def createSpace():
 	'''
@@ -144,49 +161,47 @@ def createSpace():
 	'''
 
 	# Create a black window
-	winWidth = 1400
-	winHeight = 800
-	win = gr.GraphWin("The Solar System", winWidth, winHeight, autoflush=False)
-	win.setBackground("black")
+	spaceWidth = 1400
+	spaceHeight = 800
+	space = gr.GraphWin("The Solar System", spaceWidth, spaceHeight, autoflush=False)
+	space.setBackground("black")
 
-	return win
+	return space
 
-def createSun(win):
+def createSun(space):
 	'''
 	Creates a Circle object to represent the Sun in the center of the window.
 	
-	Parameters: a GraphWin object
+	Parameters: requires Space (a GraphWin object returned from createSpace)
 	Returns: a Circle object at the center of the window
 
 	'''
+	# Find the center of the window
+	sunX = space.getWidth() / 2
+	sunY = space.getHeight() / 2
 
-	xCenter = win.getWidth() / 2
-	yCenter = win.getHeight() / 2
-	pCenter = gr.Point(xCenter, yCenter)
+	# Instantiate Sun object
+	sun = Sun((sunX, sunY), 20, "yellow", "Sun", 1.989e30)
+	name = sun.getName()
 
-	radius = 20
-	sun = gr.Circle(pCenter, radius)
-	name = gr.Text(pCenter, "Sun")
-	name.setSize(14)
-
-	sun.setFill("yellow")
-	sun.draw(win)
-	name.draw(win)
+	sun.draw(space)
+	name.draw(space)
 
 	return sun
 
-def createInnerPlanets(sun):
+def createPlanets(sun):
 	'''
-	Creates a Planet object to represent Mars and animates Mars' orbital motion around the Sun.
+	Creates Planet objects.
 
-	Parameters: GraphWin object, object at center of orbit
-	Returns: N/A
+	Parameters: requires the Sun (a Circle object returned from createSun)
+	Returns: Planet objects
 
 	'''
-
+	# Find the center of the Sun
 	solar_centerX = sun.getCenter().getX()
 	solar_centerY = sun.getCenter().getY()
 
+	# Create Planet objects
 	mercury_x = solar_centerX + 70
 	mercury_y = solar_centerY
 	mercury = Planet((mercury_x, mercury_y), 5, "grey", "Mercury", 57.91e9, 0.2056, "lightgrey", 3.3011e23, 5.898e4)
@@ -205,73 +220,83 @@ def createInnerPlanets(sun):
 
 	return mercury, venus, earth, mars
 
+def animateOrbits(space, sun):
+	'''
+	Animates the orbital motion of the planets.
 
+	Parameters: requires Space (a GraphWin object returned from createSpace)
+	and the Sun (a Circle object returned from createSun)
+	Returns: N/A
 
-def animateOrbits(win, sun):
+	'''
 
-	planet_list = [*createInnerPlanets(sun)]
+	# Create a list containing all of the Planet objects
+	planet_list = [*createPlanets(sun)]
 
+	# Initialize the key variable as an empty string
 	key = ""
 
-	key = win.getKey()
+	# Detect key press
+	key = space.getKey()
+
+	# When the user presses "Return", draw the planets in the window
 	if key == "Return":
 		for planet in planet_list:
-			planet.enable(win)
+			planet.enable(space)
+		
+		# Call the orbit function for each planet in the list of planets until the user presses "Escape"
 		while key != "Escape":
-			key = win.checkKey()
+			key = space.checkKey()
 			for planet in planet_list:
 				planet.orbit(sun)
+			
+			# Update the window to show the motion
 			gr.update(60)
 
-
-				
-
-
-def welcome_screen(win):
+def welcome_screen(space):
 	'''
-	Display a welcome screen with instructions.
+	Displays a welcome screen with instructions.
 
-	Parameters: a GraphWin object
+	Parameters: requires Space (a GraphWin object returned from createSpace)
 	Returns: N/A
 
 	'''
 
 	# Instructions for the user
-	instructions = "Click to start"
-	instructions += "\n\nChoose between Inner (Mercury - Mars) and Outer (Jupiter - Neptune) Planets"
-	instructions += "\n\nPress 'i' for Inner Planets"
-	instructions += "\n\nPress 'o' for Outer Planets"
-	instructions += "\n\nPress 'return' to orbit"
-	instructions += "\n\nPress 'esc' to quit"
+	instructions = "Welcome to the Solar System!"
+	instructions += "\n\nTo begin, click anywhere in the window."
+	instructions += "\n\nPress 'return' to simulate the orbital motion of the planets."
+	instructions += "\n\nPress 'esc' to end the simulation."
+	instructions += "\n\nClick to close the window."
 
 	# Display the instructions in a box with a solid background
-	x_center = win.getWidth()/2
-	y_center = win.getHeight()/2 - win.getHeight()*0.10
-	x_top_left = x_center - win.getWidth()/3
-	y_top_left = y_center - win.getHeight()/8
-	x_bottom_right = x_center + win.getWidth()/3
-	y_bottom_right = y_center + win.getHeight()/8
+	x_center = space.getWidth()/2
+	y_center = space.getHeight()/2 - space.getHeight()*0.10
+	x_top_left = x_center - space.getWidth()/3
+	y_top_left = y_center - space.getHeight()/8
+	x_bottom_right = x_center + space.getWidth()/3
+	y_bottom_right = y_center + space.getHeight()/8
 	box = gr.Rectangle( gr.Point(x_top_left, y_top_left), gr.Point(x_bottom_right, y_bottom_right) )
 	box.setFill("white")
 	box.setOutline("black")
-	box.draw( win )
+	box.draw(space)
 	message = gr.Text( gr.Point(x_center,y_center), str(instructions) )
 	message.setSize( 14 )
 	message.setFace("helvetica")
-	message.draw( win )
-	win.update()
+	message.draw(space)
+	space.update()
 
 	# Wait for the user to click before making the instructions disappear
-	win.getMouse()
+	space.getMouse()
 	message.undraw()
 	box.undraw()
-	win.update()
+	space.update()
 
 if __name__ == "__main__":
-	win = createSpace()
-	welcome_screen(win)
-	sun = createSun(win)
-	createInnerPlanets(sun)
-	animateOrbits(win, sun)
-	win.getMouse()
-	win.close()
+	space = createSpace()
+	welcome_screen(space)
+	sun = createSun(space)
+	createPlanets(sun)
+	animateOrbits(space, sun)
+	space.getMouse()
+	space.close()
